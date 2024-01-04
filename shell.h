c3fa39aa7461613071206549d1c56b295a6ab93d
Tmwangiw <terry.wairimu123@gmail.com>
2024-01-04 03:49:13 -0800

    Second commit

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+
+extern char **environ;
+
+void shell_interactive(void);
+void shell_non_interactive(void);
+
+#endif
